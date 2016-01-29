ff6dfde0a755405bdccd6d4226e58437fe43a96d
dmfarcas <dan@farcas.me>
2016-01-29 19:57:56 +0200

    Better(ish) contact loading, pretty contacts that don't do anything yet.

--- assets/angular/js/controllers.js
+++ assets/angular/js/controllers.js
@@ -2,41 +2,62 @@
 
 angular.module('telekinesisServer.controllers', [])
 
-.controller('notificationsCtrl', function($scope) {
+.controller('notificationsCtrl', ($scope) => {
     $scope.message = 'This is a test';
 
 })
 
-.controller('contactsCtrl', function($scope, $timeout) {
+.controller('contactsCtrl', ($scope, $timeout, $mdDialog) => {
   const ipcRender = require("electron").ipcRenderer;
+
+  //create an initialized value and check if 'contactsinit' has been run before
+  let initialized = false;
   $scope.contacts = [];
   // $scope.message = $scope.contacts;
-  ipcRender.on('contactsinit', function(event, contact) {
+
+  // Ask for contacts via IPC, maybe they are already stored
+  ipcRender.send('gimmecontacts');
+
+  ipcRender.on('contactsinit', (event, contact) => {
     $timeout(function() {
-      $scope.contacts = contact;
+        for (let i=0; i<contact[0].length; i++) {
+          $scope.contacts[i] = contact[0][i];
+        }
   },0);
     });
-    $scope.$watch('contacts', function(newValue, oldValue) {
+    $scope.$watch('contacts', (newValue, oldValue) => {
         console.log("Changed"  + newValue + " " + oldValue);
         // $scope.contacts = newValue;
-        console.log($scope.contacts[100]);
+        // console.log($scope.contacts[0]);
     });
-    $scope.sendMessage = function() {
-      console.log("Works.");
-    };
+
+    $scope.sendMessageForm = ($event) => {
+    $mdDialog.show({
+        targetEvent: $event,
+        clickOutsideToClose: true,
+        controller: DialogController,
+        templateUrl: 'html/sendMessage.html',
+        locals: { clickedname: $scope.contacts.displayName }
+        // .title('Send a message')
+        // .textContent('Send a message to ' + person)
+        // .ariaLabel('Send message')
+        // .ok('Send!')
+        // .targetEvent(event)
+  });
+  };
+
+  function DialogController($scope, $mdDialog) {
+  $scope.hide = function() {
+    $mdDialog.hide();
+  };
+  $scope.cancel = function() {
+    $mdDialog.cancel();
+  };
+  $scope.send = function(name, message) {
+    console.log(name + " " + message);
+    $mdDialog.hide(message);
+  };
+}
+
 
 });
-
-//
-//
-// .directive('keypressEvents', [
-//   '$document',
-//   'socket',
-//   function($document, socket) {
-//     return {
-//       restrict: 'A',
-//       link: function() {
-//       }
-//     }
-//   }
-// }]
--- assets/contactslistener.js
+++ assets/contactslistener.js
@@ -1,4 +1,5 @@
 "use strict";
+const ipcMain = require('electron').ipcMain;
 
 let contact = [];
 
@@ -6,7 +7,15 @@ exports.listen = function(io, mainWindow) {
 	io.on('connection', function(socket) {
 		socket.on('contacts', function(response) {
 			mainWindow.webContents.send('contactsinit', response.contact); //trying to send a message to angular via ipc, aaaargh
-			// contact.push(response.contact);
+			contact.push(response.contact);
 		});
 	});
+
+	//the Angular page will ask ipc for stored array on refresh. there should be a better way though.
+	ipcMain.on('gimmecontacts', function() {
+		mainWindow.webContents.send('contactsinit', contact);
+		console.log("Sending contacts to Angular.");
+	});
+
+
 };
