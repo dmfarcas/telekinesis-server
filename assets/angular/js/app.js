ff24defc7b74e78f14b087225a9347bef743d6d9
dmfarcas <dan@farcas.me>
2016-01-31 20:54:30 +0200

    Better search box

--- assets/angular/js/app.js
+++ assets/angular/js/app.js
@@ -1,34 +1,39 @@
 'use strict';
 
 angular.module('telekinesisServer', ['ngMaterial', 'ngRoute', 'telekinesisServer.controllers'])
-.config(function($mdThemingProvider) {
-  $mdThemingProvider.theme('default')
-    .primaryPalette('blue-grey');
-})
+	.config(function($mdThemingProvider) {
+		$mdThemingProvider.theme('default')
+			.primaryPalette('blue-grey');
+	})
 
-.factory('Page', function(){
-  var title = 'default';
-  return {
-    title: function() { return title; },
-    setTitle: function(newTitle) { title = newTitle; }
-  };
+.factory('Page', function() {
+	var title = 'default';
+	return {
+		title: function() {
+			return title;
+		},
+		setTitle: function(newTitle) {
+			title = newTitle;
+		}
+	};
 })
 
 
 
 .config(['$routeProvider',
-  function($routeProvider) {
-    $routeProvider.
-      when('/notifications', {
-        templateUrl: 'html/notifications.html',
-        controller: 'notificationsCtrl'
-    }).
+	function($routeProvider) {
+		$routeProvider.
+		when('/notifications', {
+			templateUrl: 'html/notifications.html',
+			controller: 'notificationsCtrl'
+		}).
 
-      when('/contacts', {
-        templateUrl: 'html/contacts.html',
-        controller: 'contactsCtrl'
-      }).
-      otherwise({
-        redirectTo: '/contacts'
-      });
-}]);
+		when('/contacts', {
+			templateUrl: 'html/contacts.html',
+			controller: 'contactsCtrl'
+		}).
+		otherwise({
+			redirectTo: '/contacts'
+		});
+	}
+]);
